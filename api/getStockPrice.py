61f1b67bb45ac5d3a0fb327c86984b5c8c76810b
Josh <jostone707@gmail.com>
2021-09-08 00:07:21 -0400

    testing for PAT needed for verification

--- api/getStockPrice.py
+++ api/getStockPrice.py
@@ -3,22 +3,25 @@ from datetime import datetime, timedelta
 
 # Function to get current price & intra day highs and lows & % change for a given ticker
 def getStockPrice(sym):
-    priceList = []
+    # priceList = []
+    # close = []
 
     # Get info about the current symbol
-    stock = yf.Ticker(sym)
     try:
+        stock = yf.Ticker(sym)
         # Block to get daily high and daily low stock price
-        # todays_data = stock.history(period='1d')
-        # formatted_data = "{:.2f}".format(todays_data['Close'][0])
+        todays_data = stock.history(period='1d')
+        formatted_data = "{:.2f}".format(todays_data['Close'][0])
         # formattedLow = "{:.2f}".format(stock.info['dayLow'])
         # formattedHigh = "{:.2f}".format(stock.info['dayHigh'])
 
+        print("DATA: " + formatted_data)
+
         #Block to get today's date and yesterday's date used to get yesterday's close price below
-        end = datetime.today().strftime('%Y-%m-%d')
-        start = datetime.today() - timedelta(days=1)
-        start = start.strftime('%Y-%m-%d')
-        print(type(start))
+        # end = datetime.today().strftime('%Y-%m-%d')
+        # start = datetime.today() - timedelta(days=1)
+        # start = start.strftime('%Y-%m-%d')
+
         # day = int(end[8:10])
         # day -= 1
         # day = str("%02.0f" % day)
@@ -26,6 +29,8 @@ def getStockPrice(sym):
         # start = datetime.today().strftime('%Y-%m-%d')
         # start = start[0:8]
         # start = start + day
+
+        return formatted_data
 
     # Case where the symbol retrieved is not a real stock symbol
     # Opens the commonEnglish words file and appends the bad symbol
@@ -39,33 +44,39 @@ def getStockPrice(sym):
         # fileObj.close()
 
     # Gets yesterday close price
-    print("Start = " + start + ", end = " + end)
+    # print("Start = " + start + ", end = " + end)
     # print(type(start))
 
 
-    close = 0.0
-    try:
-        close = stock.history(start=start, end=end, interval='1d')
-    except:
-        print("weekend")
-        # d = datetime.today() - timedelta(days=1)
-        # print("d = " + str(d))
-        return 0.0
-
-    print("CLOSE = " + close)
-    temp = float(close['Close'])
-    formatted_close = "{:.2f}".format(temp)
-    # Calculate the +/- percentage
-    difference = ((float(formatted_data) - float(formatted_close)) / float(formatted_data)) * 100
-    formatted_difference = "{:.2f}".format(difference)
-
-    # If statement to print a - or + sign in front of the percentage move
-    if float(formatted_difference) > 0:
-        print("    Current: $" + str(formatted_data) + ", Intra-Day Range: $" + str(formattedLow) + "-$" + str(formattedHigh) + ", Daily Difference: +" + formatted_difference + "%")
-        return formatted_data
-    else:
-        print("    Current: $" + str(formatted_data) + ", Intra-Day Range: $" + str(formattedLow) + "-$" + str(formattedHigh) + ", Daily Difference: " + formatted_difference + "%")
-        return formatted_data
+    # try:
+    #     print("BEFORE CLOSE")
+    #     close = stock.history(start=start, end=end, interval='1d')
+    #     print("AFTER CLOSE")
+    # except:
+    #     print("weekend")
+    #     # d = datetime.today() - timedelta(days=1)
+    #     # print("d = " + str(d))
+    #     return 0.0
+    #
+    # try:
+    #     print("finaly block")
+    #
+    #     temp = float(close['Close'])
+    #     formatted_close = "{:.2f}".format(temp)
+    #     # Calculate the +/- percentage
+    #     difference = ((float(formatted_data) - float(formatted_close)) / float(formatted_data)) * 100
+    #     formatted_difference = "{:.2f}".format(difference)
+    #
+    #     # If statement to print a - or + sign in front of the percentage move
+    #     if float(formatted_difference) > 0:
+    #         print("    Current: $" + str(formatted_data) + ", Intra-Day Range: $" + str(formattedLow) + "-$" + str(formattedHigh) + ", Daily Difference: +" + formatted_difference + "%")
+    #         return formatted_data
+    #     else:
+    #         print("    Current: $" + str(formatted_data) + ", Intra-Day Range: $" + str(formattedLow) + "-$" + str(formattedHigh) + ", Daily Difference: " + formatted_difference + "%")
+    #         return formatted_data
+    # except:
+    #     print("Something went wrong, fix?")
+    #     return 0.0
 
 
 if __name__ == '__main__':
